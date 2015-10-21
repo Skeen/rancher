0f565af9a04b086310d1e4922de32fec48ac8bb0
Emil 'Skeen' Madsen <sovende@gmail.com>
2015-10-21 17:55:33 +0200

    Initial support for i686 (intial work for enabling eventual armv7 support)

--- agent/build-image.sh
+++ agent/build-image.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+ARCH=$(uname -m)
+DOCKERFILE=Dockerfile-$ARCH
+
 cd $(dirname $0)
 
 if [ -z "$TAG" ]; then
-    TAG=$(grep RANCHER_AGENT_IMAGE Dockerfile | cut -f2 -d:)
+    TAG=$(grep RANCHER_AGENT_IMAGE $DOCKERFILE | cut -f2 -d:)
+    TAG=$(echo ":$TAG-$ARCH")
 fi
 
-IMAGE=rancher/agent:${TAG}
+IMAGE=rancher/agent${TAG}
 
 echo Building $IMAGE
-docker build -t ${IMAGE} .
+docker build -t ${IMAGE} -f $DOCKERFILE .
